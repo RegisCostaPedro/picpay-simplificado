7ef5727a3329c3eb1560bc5c3df7faa834df0aa4
regis <regiscostapedro0705@gmail.com>
2024-01-24 14:25:38 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.picpaysimplificado.domain.transaction;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import  com.picpaysimplificado.domain.user.User;
+@Entity(name="transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timeStamp;
+
+}
